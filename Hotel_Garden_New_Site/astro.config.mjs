68a7aa9c91e5416230255792152c6536f953dfb2
Vkscoma <vkscoma@gmail.com>
2023-06-19 07:38:22 -0700

    updating astro config

--- Hotel_Garden_New_Site/astro.config.mjs
+++ Hotel_Garden_New_Site/astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
   site: 'https://vkscoma.github.io',
   base: '/BandSiteAstro',
 });
